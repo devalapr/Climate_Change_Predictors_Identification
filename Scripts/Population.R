cdb9b0c1e5cc3d8996ba1ef5b5d19739f583cbaf
Ravi Theja <rtgunti@gmail.com>
2018-12-16 04:22:42 +0100

    Corrected Preprocessing script

--- Scripts/Population.R
+++ Scripts/Population.R
@@ -13,6 +13,7 @@ head(TemperaturDataSet)
 PopulationDataset_02_Comments_Removed$`Country Name`<-NULL
 PopulationDataset_02_Comments_Removed$`Indicator Name`<-NULL
 PopulationDataset_02_Comments_Removed$`Indicator Code`<-NULL
+PopulationDataset_02_Comments_Removed<-PopulationDataset_02_Comments_Removed[!(PopulationDataset_02_Comments_Removed$`Country Code`=="INX"),]
 TemperaturDataSet$MinTemperature<-NULL
 TemperaturDataSet$MaxTemperature<-NULL
 #colnames(PopulationDataset_02_Comments_Removed)
@@ -25,28 +26,44 @@ colnames(Population_Data)[2]="Year"
 colnames(Population_Data)[3]="Population"
 Population_Data <- Population_Data[order(Population_Data$Country),]
 #Population_Data[is.na(Population_Data)] <- 0
- Merged <- merge(Population_Data,TemperaturDataSet,by=c("Country","Year"))
+# Merged <- merge(Population_Data,TemperaturDataSet,by=c("Country","Year"))
 # head(Merged)
 
-Country_List <- unique(Merged$Country)
+Country_List <- unique(Population_Data$Country)
 #Country_List <- c("ERI")
 
 Population_final = Population_Data[FALSE,]
 for (Country1 in Country_List) {
+  cat("Current Country is ", Country1)
   Population_Country <- Population_Data %>% filter(Country == Country1)
+  #cat("Rows in Population_Country",nrow(Population_Country),"\n")
+  #print(Population_Country)
   temp_country <- na.omit(Population_Country)
-  values <- lm(temp_country$Population ~ temp_country$Year, temp_country)
-  print(summary(values))
+  #cat("Data after ommiting null values","\n")
+  # print(temp_country)
+  # transform(temp_country, Year = as.numeric(Year))
+  temp_country$Year <- as.numeric(as.character(temp_country$Year))
+  Population_Country$Year <- as.numeric(as.character(Population_Country$Year))
+  Population_Data$Year <- as.numeric(as.character(Population_Data$Year))
+  #cat("Check for factor ",is.factor(temp_country$Year))
+  #print(temp_country$Year)
+  linearMod <- lm(Population ~ Year, data=temp_country)
+  print(summary(linearMod))
+  cat("Liner Model Coeffs",linearMod$coefficients,"\n")
   for(i in 1:nrow(Population_Country))
   {
     if(is.na(Population_Country$Population[i]))
     {
-      Population_Country$Population[i] = as.numeric(values$coef[1]) + as.numeric(values$coef[2])*as.numeric(Population_Data$Year[i])
-    }
+      cat("Yep Yep Johnny Depp",i,"\n")
+      Population_Country$Population[i] = as.numeric(linearMod$coefficients[1]) + as.numeric(linearMod$coefficients[2])*(Population_Data$Year[i])
+      #cat(as.numeric(as.numeric(Population_Country$Population[i]),"=",linearMod$coefficients[1]),as.numeric(linearMod$coefficients[2]),as.numeric(Population_Data$Year[i]))
+      #cat("Year",Population_Data$Year[i])
+      cat("\n")
+      }
   }
   Population_final <- rbind(Population_final,Population_Country)
 }
-head(Population_final)
+#head(Population_final)
 write.csv(Population_final,file="Data sets/PopulationDataset_03_Preprocessed.csv",row.names=FALSE)
 #head(Population_Data)
 #write.csv(Population_Data,file="Data sets/PopulationDataset.csv",row.names=FALSE)

f34dde23037e54681a4f5fe7bb767fce6daae4c7
Rakesh Devalapally <rakesh.devalapally@st.ovgu.de>
2019-01-12 12:24:26 +0100

    Added appropriate comments

--- Scripts/PPScript_GDPDS.R
+++ Scripts/PPScript_GDPDS.R
@@ -1,41 +1,37 @@
 library(readr)
 library(reshape2)
-library(ggplot2)
 library(dplyr)
-library(sqldf)
 library(tibble)
-#uncomment this code for generating the data set csv from the original data set
-print(gdpAbsolute)
-gdp_data<- melt(gdpAbsolute, id=c("Country Name","Country Code","Indicator Name","Indicator Code"))
-#print(gdp_data)
-#write.csv(gdp_data,file="GDPDataSet.csv")
+#Read the original dataset
+gdpAbsolute <- read.csv("GDPAbsolute.csv")
+
+# melting the data to the required format
+gdp_data <- melt(gdpAbsolute, id=c("Country Name","Country Code","Indicator Name","Indicator Code"))
 
 #formatting the data set to rename the columns from the generated data set for column names variable and value to year and gdp
-#GDPDataSet <- read_csv("Data sets/GDPDataSet.csv")
 GDPDataSet <- gdp_data
-GDPDataSet<-GDPDataSet[-c(1)]
-colnames(GDPDataSet)[5]<-"Year"
-colnames(GDPDataSet)[6]<-"GDP"
+GDPDataSet <- GDPDataSet[-c(1)]
+colnames(GDPDataSet)[5] <- "Year"
+colnames(GDPDataSet)[6] <- "GDP"
 
-#Gdp data set with arregated gdp 
+#Aggregate the GDP by grouping at country and year level
 GDPDataSetGroup <- GDPDataSet %>% 
                     group_by(`Country Name`,`Country Code`,Year) %>%
                       summarize(GDP = sum(GDP))
-#Getting the list of countries with available number of gdp less than 20 years
-Gdp_OmitCountryList=sqldf('select "Country Name","Country Code",count(year) as DataCount from GDPDataSetGroup where GDP!="NA" group by "Country Name"')
-Gdp_OmitCountryList=as.data.frame(Gdp_OmitCountryList)
+
+#Getting the list of countries with available number of datapoints for gdp less than 20 years
+Gdp_OmitCountryList = sqldf('select "Country Name","Country Code",count(year) as DataCount from GDPDataSetGroup where GDP!="NA" group by "Country Name"')
+Gdp_OmitCountryList = as.data.frame(Gdp_OmitCountryList)
+
 #filtering the list to get only country codes
 Gdp_OmitCountryList = Gdp_OmitCountryList %>% 
   filter(Gdp_OmitCountryList[,3] <20) %>% select(`Country Code`)
+
 #creating an array equal to the length of the gdp_omitcountrylist for running it as a for loop for filtering  purpose
-GdpOmitListCount<-array(1:NROW(Gdp_OmitCountryList))
+GdpOmitListCount <- array(1:NROW(Gdp_OmitCountryList))
 for (rowIndex in GdpOmitListCount) {
-cab<-Gdp_OmitCountryList[rowIndex,1]
-GDPDataSet=GDPDataSet %>% filter(GDPDataSet[,4]!=cab)
-
+  cab <- Gdp_OmitCountryList[rowIndex,1]
+  GDPDataSet = GDPDataSet %>% filter(GDPDataSet[,4]!=cab)
 }
-View(GDPDataSet)
-
-write.csv(GDPDataSet,file="GDPDataSet.csv",row.names = FALSE)
-
-
+# Write the final data frame/set into a new file
+write.csv(GDPDataSet, file = "PreProcessed/GDPDataSet.csv", row.names = FALSE)
